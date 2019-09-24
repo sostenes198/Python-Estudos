a220802dbe7f75198780b369d922538a54f643a6
Sostenes Goncalves de Souza <sostenes.goncalves@squadra.com.br>
2019-09-24 09:22:52 -0300

    Adiciona estudos python

---
+++ Estudos-Blockchain/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'estudos-blockchain',
+    version = '1.0',
+    description = 'Estudos simples sobre blockchain',
+    url = 'https://github.com/sostenes198/Python-Estudos',
+    author = 'Sóstenes Gonçalves',
+    author_email = 'sostenes198br@hotmail.com',
+    licence = 'MIT',
+    packages = ['blockchain'],
+    zip_safe = False
+)
